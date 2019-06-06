5e8f4c836d3577b5670fd71750492dd7fc82785d
Микита Маркевич <mykyta.markevych@gmail.com>
2019-06-07 00:02:49 +0300

    fjfj

--- bot.py
+++ bot.py
@@ -14,13 +14,6 @@ bot = telebot.TeleBot(token)
 server = Flask(__name__)
 now = datetime.datetime.now
 
-
-"""class randomrow:
-    def __init__(self,rownum):
-        self.rownum = rownum
-    def rand(self):
-        return random.randint(1,201) #жуткий костыль
-"""
 @server.route('/' + token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -32,21 +25,7 @@ def webhook():
     bot.set_webhook(url='https://polar-journey-94573.herokuapp.com/' + token)
     return "?", 200 
     
-"""@bot.message_handler(commands=['start'])
-def start(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    user_markup.row('Почати')
-    bot.send_message(message.chat.id, 'Щоб ввести номер телефону оберіть /search')
 
-@bot.message_handler(commands=['search'])
-def game(message):
-    bot.send_message(message.chat.id, 'номер без 0')
-    answer = utils.get_answer_for_user(message.chat.id)
-    db_worker = SQLighter(database_name)
-    rows = db_worker.select_single(answer) 
-    bot.send_message(message.chat.id, rows)
-    db_worker.close()
-"""
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/tel":
@@ -66,20 +45,7 @@ def get_text_messages(message):
                     bot.send_message(message.chat.id, (result.fetchall())
                 
         
-"""
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def check_answer(message):
-    answer = utils.get_answer_for_user(message.chat.id)
-    if not answer:
-        bot.send_message(message.chat.id, 'Щоб почати наступне питання, оберіть команду /test')
-    else:
-        keyboard_hider = telebot.types.ReplyKeyboardRemove()
-        if message.text == answer:
-            bot.send_message(message.chat.id, "Так! Далі - /test", reply_markup=keyboard_hider)
-        else:
-            bot.send_message(message.chat.id, "Ні, правильна відповідь: %s. Далі - /test" %answer, reply_markup=keyboard_hider)
-        utils.finish_user_game(message.chat.id)
- """         
+
 if __name__ == '__main__':
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80)) 
 
